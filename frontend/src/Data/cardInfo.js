8074ac8d9c7d36702d2a910db13c3dddb2975074
JohnPLeg <kniimare@gmail.com>
2025-08-12 17:18:25 -0400

    altered styling of cards on frontpage

--- frontend/src/Data/cardInfo.js
+++ frontend/src/Data/cardInfo.js
@@ -1,13 +1,19 @@
 export const cardInfo = [
     {
         title: "Stat Block Creator",
-        description: "Use this tool to create your very own custom stat blocks",
+        description: "Use this tool to create your very own custom stat blocks!",
         image: "zombieStats.png",
         route: "/stat-select"
     },
     {
+        title: "Initiative Tracker",
+        description: "Track initiative count, armor class, and health",
+        image: "d20.png",
+        route: "/initiative"
+    },
+    {
         title: "Coming Soon",
-        description: "New features like an initiative counter are being worked on",
+        description: "New features are being looked into!",
         image: "questionMark.png",
         route: ""
     }
