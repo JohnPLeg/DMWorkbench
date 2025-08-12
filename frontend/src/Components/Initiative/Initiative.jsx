ba64f19c26b849ad1b2f087c1531226d714c7b52
JohnPLeg <kniimare@gmail.com>
2025-08-12 17:11:01 -0400

    finished initiative tracker

--- frontend/src/Components/Initiative/Initiative.jsx
+++ frontend/src/Components/Initiative/Initiative.jsx
@@ -3,17 +3,54 @@ import { useState } from 'react';
 import Navigation from '../Navigation/Navigation';
 
 function Initiative() {
+    const [monsterList, setMonsterList] = useState([
+        {
+            name: 'Example',
+            count: 0,
+            health: 0,
+            armor_class: 0
+        }
+    ]);
+    const [highlighted, setHighlighted] = useState(-1)
     const emptyCreature = {
         name: '',
         count: '',
         health: '',
         armor_class: ''
     }
+    const [round, setRound] = useState(0);
     
     function compare(a, b) {
         if (a.count < b.count) return -1;
         if (a.count > b.count) return 1;
         return 0;
+    }
+
+    const handleNext = () => {
+        if (monsterList.length > 0) {
+                setHighlighted(prev => {
+                const nextIndex = prev + 1;
+                if (nextIndex === monsterList.length) {
+                    setRound(prevRound => prevRound + 1);
+                    return 0;
+                } else {
+                    return nextIndex;
+                }
+            });
+        } else {
+            alert("Must have at least 1 creature!");
+        }
+    }
+
+    const handleRoundReset = () => {
+        setRound(0);
+        setHighlighted(-1);
+    }
+
+    const handleRemove = (idx) => {
+        setMonsterList(prev => (
+            prev.filter((_, index) => index !== idx)
+        ))
     }
 
     const handleSort = () => {
@@ -29,19 +66,10 @@ function Initiative() {
     }
 
     const handleClear = () => {
-        setMonsterList([{
+        setMonsterList([
             emptyCreature
-        }])
+        ])
     }
-
-    const [monsterList, setMonsterList] = useState([
-        {
-            name: 'Example',
-            count: 0,
-            health: 0,
-            armor_class: 0
-        }
-    ]);
 
     const handleUpdate = (newValue, idxToUpdate, field) => {
         setMonsterList(prevList => (
@@ -58,15 +86,15 @@ function Initiative() {
         <>
             <Navigation/>
             <div className={styles.trackerContainer}>
-                <div className={styles.titles}>
-                    <h4 className={styles.name} >Name:</h4>
+                <div className={styles.gridRow}>
+                    <h4>Name:</h4>
                     <h4>Initiative:</h4>
                     <h4>Health:</h4>
                     <h4>Armor Class:</h4>
                 </div>
                 <div className={styles.inputContainer}>
                     {monsterList.length > 0 && monsterList.map((creature, index) => (
-                        <div className={styles.initInputs} key={index}>
+                        <div className={styles.gridRow} key={index} style={{backgroundColor: highlighted === index ? 'rgba(47, 99, 79, 1)' : ''}}>
                             <input 
                                 type='text' 
                                 value={creature.name} 
@@ -95,15 +123,20 @@ function Initiative() {
                                 onChange={(e) => handleUpdate(e.target.value, index, 'armor_class')}
                                 className={styles.creatureAC}
                             />
+                            <button onClick={() => handleRemove(index)}>X</button>
                         </div>
                     ))}
                 </div>
                 <div className={styles.btnGrid}>
-                    <button className={styles.nextInitBtn}>Next</button>
+                    <button className={styles.nextInitBtn} onClick={handleNext}>Next</button>
                     <button className={styles.sortBtn} onClick={handleSort}>Sort</button>
                     <button className={styles.addCreatureBtn} onClick={handleAdd}>Add Creature</button>
                     <button className={styles.clearOrder} onClick={handleClear}>Clear</button>
-                    <button className={styles.resetBtn}>Reset Rounds</button>
+                    <button className={styles.resetBtn} onClick={handleRoundReset}>Reset Rounds</button>
+                    <div className={styles.roundNum}>
+                        <h2>Round:</h2>
+                        <h1>{round}</h1>
+                    </div>
                 </div>
             </div>
         </>
